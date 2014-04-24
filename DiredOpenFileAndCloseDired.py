0951b1876b42acf9c10b85e416e494032db5f88e
CarlosEDP <me@carlosedp.com>
2014-04-24 16:33:13 -0300

    Updated module. Waiting for new version of FileBrowser.

--- DiredOpenFileAndCloseDired.py
+++
@@ -1,7 +0,0 @@
-import sublime, sublime_plugin
-
-class DiredOpenFileAndCloseDired(sublime_plugin.WindowCommand):
-    def run(self):
-        self.window.run_command("dired_select")
-        self.window.run_command("next_view_in_stack")
-        self.window.run_command("close")
